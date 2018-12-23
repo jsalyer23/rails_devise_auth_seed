9aed5442d8f93f3f92c1740ae0493cef5ce6b105
Josh Salyer <joshsalyer23@gmail.com>
2018-12-23 17:06:48 -0600

    Creates user schema and example user seed, created example user with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create(email: 'testuser@example.com', nickname: 'Tester', name: 'Test User',
+#             password: 'password')
+User.create(email: 'user@example.com', nickname: 'UOne', name: 'User One', password: "monkey67")
